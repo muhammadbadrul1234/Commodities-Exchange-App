903f46ee09abb171bbb80888c8c0fe7f433c2391
muhammadbadrul1234 <badrulalomtawsyat@gmail.com>
2022-11-19 23:16:47 +0600

    Muhammad Badrul

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,16 +15,16 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    //Verify that the text is displayed
+    expect(find.text('Dhaka Stock Exchange'), findsOneWidget);
+    expect(find.text('Feel the new experience of Stock'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify that the button is displayed
+    expect(find.text('Getting Started'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the image is displayed
+    expect(find.byType(Image), findsOneWidget);
+
+
   });
 }
